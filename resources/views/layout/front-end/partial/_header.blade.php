efdac9b9ebad3ae22bb540ffb676fe86b4803d57
ameedasmah1 <ameed.a@averotech.com>
2022-06-02 14:09:46 +0300

    addingadding Header

--- resources/views/layout/front-end/partial/_header.blade.php
+++ resources/views/layout/front-end/partial/_header.blade.php
@@ -1,15 +1,15 @@
 <div dir="rtl" class="flex flex-row justify-between items-center my-4 h-14  w-full pr-20 pl-24">
     <div class="flex basis-2/5 flex-row justify-between items-center text-base text-[#101426] cursor-pointer" >
-        <a href="#">من نحن</a>
-        <a href="#">مشاريعنا</a>
-        <a href="#">اخبارنا</a>
-        <a href="#">القدس والمسجد الاقصى</a>
+        <a class="hover:text-[#349A37]" href="#">من نحن</a>
+        <a class="hover:text-[#349A37]" href="#">مشاريعنا</a>
+        <a class="hover:text-[#349A37]" href="#">اخبارنا</a>
+        <a class="hover:text-[#349A37]" href="#">القدس والمسجد الاقصى</a>
     </div>
     <img class="flex basis-2/5 max-w-[200px] max-h-[60px]" src="{{ asset('assets/image/image 1.svg') }}" />
     <div class="flex flex-row  basis-2/5 justify-between items-center text-base text-[#101426] cursor-pointer">
-        <a href="#">الاوقاف والمقدسات</a>
-        <a href="#">حصاد الجمعية</a>
-        <a href="#"> التبرع للمشاريع</a>
-        <a href="#">اتصل بنا</a>
+        <a class="hover:text-[#349A37]" href="#">الاوقاف والمقدسات</a>
+        <a class="hover:text-[#349A37]" href="#">حصاد الجمعية</a>
+        <a class="hover:text-[#349A37]" href="#"> التبرع للمشاريع</a>
+        <a class="hover:text-[#349A37]" href="#">اتصل بنا</a>
     </div>
 </div>
