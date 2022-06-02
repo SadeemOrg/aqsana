365c03912d268413c38c7519126870fd65b2855d
ameedasmah1 <ameed.a@averotech.com>
2022-06-02 14:09:13 +0300

    done with footer

--- resources/views/layout/front-end/partial/_footer.blade.php
+++ resources/views/layout/front-end/partial/_footer.blade.php
@@ -1,1 +1,38 @@
-<div class="bg-black text-fuchsia-400">footer</div>
+<div dir="rtl" class="bg-[#101426] min-h-[350px] text-white flex flex-col items-center justify-start mt-24">
+<img src="{{ asset('assets/image/Logo_footer.svg') }}" class="w-[165px] pt-10 max-h-[125px]" />
+<div class="hidden sm:flex flex-row flex-wrap justify-around items-center gap-7 pt-9">
+    <a class="text-[16px] hover:text-[#349A37]" href="#">من نحن</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">مشاريعنا</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">اخبارنا</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">القدس والمسجد الاقصى</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">الاوقاف والمقدسات</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">حصاد الحمعية</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">التبرع للمشاريع</a>
+    <a class="text-[16px] hover:text-[#349A37]" href="#">اتصل بنا</a>
+</div>
+<div class="flex flex-row justify-between gap-x-6 sm:hidden mt-6 text-right w-[80%]">
+    <div class="flex flex-col items-start justify-start text-right gap-y-2 text-[10px] ">
+        <a class="text-[14px] text-right hover:text-[#349A37]" href="#">من نحن</a>
+        <a class="text-[14px] text-right hover:text-[#349A37]" href="#">مشاريعنا</a>
+        <a class="text-[14px] text-right hover:text-[#349A37]" href="#">اخبارنا</a>
+        <a class="text-[14px] text-right hover:text-[#349A37]" href="#">القدس والمسجد الاقصى</a>
+    </div>
+    <div class="flex flex-col items-start justify-start  gap-y-2 text-[10px] ">
+        <a class="text-[14px] hover:text-[#349A37]" href="#">الاوقاف والمقدسات</a>
+        <a class="text-[14px] hover:text-[#349A37]" href="#">حصاد الحمعية</a>
+        <a class="text-[14px] hover:text-[#349A37]" href="#">التبرع للمشاريع</a>
+        <a class="text-[14px] hover:text-[#349A37]" href="#">اتصل بنا</a>
+    </div>
+</div>
+<div dir="rtl" class="flex flex-col sm:flex-row justify-between items-center mt-10 w-[80%] border-t border-t-[#727272]">
+<div class="flex flex-start items-center pt-9 gap-x-1 ">
+    <img class="cursor-pointer" src="{{ asset('assets/image/Youtube.svg') }}" alt="youtube">
+    <img class="cursor-pointer" src="{{ asset('assets/image/Twitter.svg') }}" alt="Twitter">
+    <img class="cursor-pointer" src="{{ asset('assets/image/inestegram.svg') }}" alt="inestegram">
+    <img class="cursor-pointer" src="{{ asset('assets/image/facebook.svg') }}" alt="facebook">
+</div>
+<p class="text-16px pt-9 pb-4">
+    Copyright 2022 Name By Averotech
+</p>
+</div>
+</div>
