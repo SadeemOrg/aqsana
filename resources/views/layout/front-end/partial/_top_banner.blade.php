95ebcb2fc73055a49a013732aa03a9c4db850107
ameedasmah1 <ameed.a@averotech.com>
2022-06-05 16:57:54 +0300

    add Top Banner Page

--- resources/views/layout/front-end/partial/_top_banner.blade.php
+++ resources/views/layout/front-end/partial/_top_banner.blade.php
@@ -1,1 +1,18 @@
 <img class="w-screen max-h-[500px]" src="{{ asset('assets/image/27.png') }}" alt="topBanner">
+<div dir="rtl" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex flex-row flex-wrap xl:flex-nowrap  items-center justify-center xl:justify-start  gap-x-7">
+        <div class="h-[380px] max-w-[600px] bg-[#E4FFE585] mt-16 rounded-[5px] pt-7 px-4">
+            <img src="{{ asset('assets/image/people_on_Mousq.png') }}" alt="people_on_Mousq" class="w-full max-h-60">
+            <p class="text-[#349A37] text-[18px] pt-4 text-center max-w-[90%] px-4">تحت عنوان اثر طاعة المسلم لربة..
+                <span class="text-black">جمعية الاقصى تنظم لقائها الشهري في المسجد الاقصى المبارك</span>
+            </p>
+        </div>
+        <!--second photo -->
+        <div class="h-[380px] max-w-[600px] bg-[#E4FFE585] mt-10 xl:mt-16 rounded-[5px] pt-7 px-4">
+            <img src="{{ asset('assets/image/Alquds_First.png') }}" alt="people_on_Mousq" class="w-full max-h-60">
+            <p class="text-[#349A37] text-[18px] pt-4 text-center max-w-[90%] px-4">أكثر من 10 الاف مشارك
+                <span class="text-black">بمعسكر “القدس أولًا 13” الذي نظمته الحركة الإسلامية وجمعية الأقصى</span>
+            </p>
+        </div>
+    </div>
+</div>
