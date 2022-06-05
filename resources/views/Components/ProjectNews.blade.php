e967ba37ebda5e328c868ac3b223197213f79c88
ameedasmah1 <ameed.a@averotech.com>
2022-06-05 16:58:13 +0300

    Adding ProjectNews Page

---
+++ resources/views/Components/ProjectNews.blade.php
@@ -0,0 +1,66 @@
+<style>
+    .newsShaddow:hover {
+        box-shadow: 0px 0px 1px 0px #0000000A;
+        box-shadow: 0px 2px 6px 0px #0000000A;
+        box-shadow: 0px 16px 24px 0px #0000000F;
+    }
+</style>
+<div dir="rtl" class="max-w-7xl mx-auto px-4 pt-12 sm:pt-28 sm:px-6 lg:px-8">
+    <p class="font-FlatBold text-3xl text-center xl:text-right">أخبار المشاريع</p>
+    <div class="flex flex-row flex-wrap items-center justify-start sm:justify-center xl:items-start xl:justify-between gap-y-6 xl:gap-y-0">
+        <div class="flex flex-col items-center basis-full sm:basis-3/5 xl:basis-2/5 gap-y-5">
+            <div
+                class="newsShaddow flex flex-row justify-between py-6 items-center w-full text-lg font-FlatBold text-right text-[#101426] mt-14  bg-white px-4">
+                <p>
+                    أكثر من 10 آلاف مشارك بمعسكر “القدس أولًا 13”
+                </p>
+                <svg class="hidden md:block" width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M0.654771 8.49998C0.654771 8.19531 0.771103 7.89067 1.00328 7.65839L8.31297 0.348771C8.77795 -0.116216 9.53185 -0.116216 9.99665 0.348771C10.4614 0.81357 10.4614 1.56732 9.99665 2.03234L3.52863 8.49998L9.99642 14.9677C10.4612 15.4326 10.4612 16.1863 9.99642 16.6511C9.53162 17.1163 8.77773 17.1163 8.31274 16.6511L1.00305 9.34158C0.770839 9.10918 0.654771 8.80454 0.654771 8.49998Z"
+                        fill="#349A37" />
+                </svg>
+            </div>
+            <!--second News -->
+            <div
+                class="newsShaddow flex flex-row items-center py-6 justify-between w-full  text-[16px]  font-FlatBold text-right text-[#101426] bg-white px-4">
+                <p class="max-w-[390px]">
+                    جمعية الأقصى- الحركة الإسلامية تطلق 70 حافلة للأقصى وتنظم العديد من الفعاليات إحياءً لذكرى
+                    الإسراء والمعراج
+                </p>
+                <svg class="hidden md:block" width="11" height="17" viewBox="0 0 11 17" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M0.654771 8.49998C0.654771 8.19531 0.771103 7.89067 1.00328 7.65839L8.31297 0.348771C8.77795 -0.116216 9.53185 -0.116216 9.99665 0.348771C10.4614 0.81357 10.4614 1.56732 9.99665 2.03234L3.52863 8.49998L9.99642 14.9677C10.4612 15.4326 10.4612 16.1863 9.99642 16.6511C9.53162 17.1163 8.77773 17.1163 8.31274 16.6511L1.00305 9.34158C0.770839 9.10918 0.654771 8.80454 0.654771 8.49998Z"
+                        fill="#349A37" />
+                </svg>
+            </div>
+            <!--third News -->
+            <div
+                class="newsShaddow flex flex-row items-center py-6 justify-between w-full  text-[16px]  font-FlatBold text-right text-[#101426] bg-white px-4">
+                <p class="max-w-[390px]">
+                    تحت عنوان أثر طاعة المسلم لربه.. جمعية الأقصى تنظم لقائها الشهري في المسجد الأقصى المبارك </p>
+                </p>
+                <svg class="hidden md:block" width="11" height="17" viewBox="0 0 11 17" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M0.654771 8.49998C0.654771 8.19531 0.771103 7.89067 1.00328 7.65839L8.31297 0.348771C8.77795 -0.116216 9.53185 -0.116216 9.99665 0.348771C10.4614 0.81357 10.4614 1.56732 9.99665 2.03234L3.52863 8.49998L9.99642 14.9677C10.4612 15.4326 10.4612 16.1863 9.99642 16.6511C9.53162 17.1163 8.77773 17.1163 8.31274 16.6511L1.00305 9.34158C0.770839 9.10918 0.654771 8.80454 0.654771 8.49998Z"
+                        fill="#349A37" />
+                </svg>
+            </div>
+            <!--last News -->
+            <div
+                class="newsShaddow flex flex-row items-center py-6 justify-between w-full  text-[16px]  font-FlatBold text-right text-[#101426] bg-white px-4">
+                <p class=" xl:max-w-[390px]">
+                    تحت عنوان أثر طاعة المسلم لربه.. جمعية الأقصى تنظم <br />لقائها الشهري في المسجد الأقصى المبارك </p>
+                </p>
+                <svg class="hidden md:block" width="11" height="17" viewBox="0 0 11 17" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M0.654771 8.49998C0.654771 8.19531 0.771103 7.89067 1.00328 7.65839L8.31297 0.348771C8.77795 -0.116216 9.53185 -0.116216 9.99665 0.348771C10.4614 0.81357 10.4614 1.56732 9.99665 2.03234L3.52863 8.49998L9.99642 14.9677C10.4612 15.4326 10.4612 16.1863 9.99642 16.6511C9.53162 17.1163 8.77773 17.1163 8.31274 16.6511L1.00305 9.34158C0.770839 9.10918 0.654771 8.80454 0.654771 8.49998Z"
+                        fill="#349A37" />
+                </svg>
+            </div>
+        </div>
+        <img src="{{ asset('assets/image/Rock_Mousq.png') }}" alt="Rock_Mousq" class="py-8 mt-14 xl:max-h-[450px] basis-full sm:basis-3/5  xl:basis-2/5 ">
+    </div>
+</div>
