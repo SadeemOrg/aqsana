b1e762f5cafef2b0fc353016e2029ee823b977b6
ameedasmah1 <ameed.a@averotech.com>
2022-06-06 17:10:46 +0300

    adding About-us Page and Components

---
+++ resources/views/Components/aboutUs/AboutUsComp.blade.php
@@ -0,0 +1,23 @@
+<div dir="rtl" class="max-w-7xl mx-auto px-4 pt-12 sm:pt-28 sm:px-6 lg:px-8">
+    <div class="flex flex-row flex-wrap xl:flex-nowrap items-center justify-start sm:justify-center  bg-[#F2FFF285] xl:items-start xl:justify-between gap-y-6 xl:gap-y-0 gap-x-7">
+        <div class="flex flex-col  xl:basis-3/5 justify-center items-center xl:items-start">
+            <div class="relative mt-4 xl:mt-8">
+                <p class="font-FlatBold text-3xl text-center xl:text-right"> من نحن</p>
+                <div class="absolute border-b-[4px] pt-2 border-b-[#349A37] w-7 hidden xl:block"></div>
+            </div>
+            <img src="{{ asset('assets/image/aboutUsComp.png') }}" alt="aboutUsComp"
+            class="block xl:hidden mt-12 object-cover" />
+            <p class="max-w-2xl pt-9 font-noto_Regular text-base sm:text-lg text-justify md:text-right">جمعية الأقصى هي جمعية رسمية مسجلة منذ العام 1991 م، ترعى شؤون الأوقاف والمقدسات الإسلامية وتعمل على
+                المحافظة
+                <span class="text-yellow-400">عليها داخل الخط الأخضر(فلسطين48)</span> ، وتركز الجمعية في عملها على القدس الشريف والمسجد الأقصى
+                المبارك (أولى القبلتين، وثاني المسجدين) باعتباره درة تاجها ومن أقدس مقدسات المسلمين في فلسطين وكل ما يصب
+                في خدمته. تعمل الجمعية على متابعة قضايا الأوقاف والمقدسات العربية والإسلامية من أجل معالجتها وترميمها
+                وصيانتها وتحريرها وذلك من خلال إنجاز دراسة شاملة لجميع الأوقاف والمقدسات الإسلامية في الداخل الفلسطيني
+                لتحديد أماكنها وكيفية الوصول إليها، وقد أتمته الجمعية في السنين الأولى لتأسيسها. بالأضافة إلى تنفيذ
+                مشاريع توعوية لكافة الأجيال من فلسطينيي الداخل وربطهم تاريخياً وثقافياً واجتماعياً وروحياً بمسجدهم
+                الأقصى وسائر المقدسات. يترأس الجمعية منذ تأسيسها الشيخ كامل ريان ويديرها حالياً الشيخ صفوت فريج.</p>
+        </div>
+        <img src="{{ asset('assets/image/aboutUsComp.png') }}" alt="aboutUsComp"
+            class="xl:basis-2/5 max-w-[550px] max-h-[460px] xl:block hidden" />
+    </div>
+</div>
