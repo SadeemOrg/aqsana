06de8f86737408d38797531dfa69ba9d6c50059f
ameedasmah1 <ameed.a@averotech.com>
2022-06-06 17:10:07 +0300

    modify structural for Home Page & adding app page

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,41 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<style>
+    @layer base {
+        @font-face {
+            font-family: "alfont_com_JF-Flat-Bold";
+            font-weight: 700px;
+            src: url('/public/assets/front-end/fonts/alfont_com_JF-Flat-Bold.ttf')
+        }
+
+        @font-face {
+            font-family: "alfont_com_JF-Flat-regular";
+            font-weight: 400px;
+            src: url('/public/assets/front-end/fonts/alfont_com_JF-Flat-regular.ttf')
+        }
+
+        .font-FlatBold {
+            font-family: 'alfont_com_JF-Flat-Bold' !important
+        }
+    }
+</style>
+
+<body>
+
+    @include('layout.front-end.partial._header')
+    @yield('content')
+    @include('layout.front-end.partial._footer')
+
+
+</body>
+<!-- <script src="{{ mix('/js/app.js') }}"></script> -->
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
