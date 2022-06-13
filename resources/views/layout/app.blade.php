32b06edb7a8bc2838e6f4af3343539d7e9a9511c
ameedasmah1 <ameed.a@averotech.com>
2022-06-13 16:00:02 +0300

    adding Slider Page

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css"/>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css" />
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -35,12 +36,14 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/front-end/css/main.css') }}">
 </head>
+
 <body>
     @include('layout.front-end.partial._header')
     @yield('content')
     @include('layout.front-end.partial._footer')
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('assets/front-end/js/main.js') }}"></script>
 </body>
@@ -49,21 +52,121 @@
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <script>
-    $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
+var owl = $("#main-home-slider");
+// console.log('owl',owl[0])
+        owl.owlCarousel({
+            rtl: true,
+            autoplay: true,
+            loop:true,
+            responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:4
+            items:1
         }
     }
+        });
+        var owl1 = $("#association-news-slider");
+        owl1.owlCarousel({
+            rtl:true,
+
+    loop:true,
+    margin:20,
+    nav:true,
+    dots:true,
+    dotsClass:'dotsCurosal',
+    responsive: {
+                //X-Small
+                0: {
+                    items: 1
+                },
+                360: {
+                    items: 1
+                },
+                375: {
+                    items: 1
+                },
+                540: {
+                    items: 1
+                },
+                //Small
+                576: {
+                    items: 1
+                },
+                //Medium
+                768: {
+                    items: 1
+                },
+                850: {
+                    items: 2
+                },
+                //Large
+                1120: {
+                    items: 2
+                },
+                //Extra large
+                1200: {
+                    items: 3
+                },
+                //Extra extra large
+                1400: {
+                    items: 3
+                }
+            }
+})
+
+
+var owl2 = $("#association-news-slider-1");
+        owl1.owlCarousel({
+            rtl:true,
+
+    loop:true,
+    margin:20,
+    nav:true,
+    dots:true,
+    dotsClass:'dotsCurosal',
+    responsive: {
+                //X-Small
+                0: {
+                    items: 1
+                },
+                360: {
+                    items: 1
+                },
+                375: {
+                    items: 1
+                },
+                540: {
+                    items: 1
+                },
+                //Small
+                576: {
+                    items: 1
+                },
+                //Medium
+                768: {
+                    items: 1
+                },
+                850: {
+                    items: 2
+                },
+                //Large
+                1120: {
+                    items: 2
+                },
+                //Extra large
+                1200: {
+                    items: 3
+                },
+                //Extra extra large
+                1400: {
+                    items: 3
+                }
+            }
 })
 </script>
 
