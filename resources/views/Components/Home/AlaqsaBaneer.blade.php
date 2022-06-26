3b0e3932f4e41fb96603b957665a087d9e9b34e8
ameedasmah1 <ameed.a@averotech.com>
2022-06-26 12:04:04 +0300

    connecting data with Home Page

--- resources/views/Components/Home/AlaqsaBaneer.blade.php
+++ resources/views/Components/Home/AlaqsaBaneer.blade.php
@@ -2,35 +2,32 @@
 <style>
     .linearGradiants {
         background: linear-gradient(269.95deg, rgba(16, 20, 38, 0.8) 0.03%, rgba(16, 20, 38, 0.54) 84.34%, rgba(16, 20, 38, 0) 99.95%);
-
     }
 </style>
-
 @php
-    $img = 'storage/' .nova_get_setting('main_img_Banner_1', 'default_value');
-    // dd($img)
+    $img = 'storage/'.nova_get_setting('main_img_Banner_1', 'default_value');
+    $main_text = nova_get_setting('text_main_Banner_1', 'default_value');
+    $sub_Text=nova_get_setting('sup_text_Banner_1', 'default_value');
+    $youtube_Logo=nova_get_setting('youtube', 'default_value');
+    $logo_Banner_Img = 'storage/'.nova_get_setting('logo_Banner_1', 'default_value')
+
 @endphp
 <div class="relative mt-24 md:max-h-72 min-h-[294px] overflow-hidden">
     <div class="absolute inset-0">
-        <img class="w-full h-full object-cover" src="{{URL::asset($img)}}" alt="AlAqsaBanner">
+        <img class="w-full h-full object-cover" src="{{ asset('assets/image/AlAqsaBanner.png') }}" alt="AlAqsaBanner">
 
         <div class="absolute inset-0  linearGradiants mix-blend-multiply" aria-hidden="true"></div>
     </div>
     <div class="relative max-w-7xl mx-auto sm-py-12 py-8 px-4 sm:px-6 lg:px-8 md:pt-16">
         <div class="flex flex-row flex-wrap items-center justify-center gap-y-2 md:justify-between ">
             <div class="text-right font-FlatBold text-white">
-                <p class="text-[18px] md:text-[22px] pb-4  ">قم بالتبر ع للعديد من المشاريع التابعة للجمعية</p>
-                <p class="text-[12px] md:text-[14px] max-w-lg ">قال رسول الله صلى الله عليه وسلم: ((كل سُلَامَى من الناس
-                    عليه صدقةٌ،
-                    كل يومٍ تطلع فيه الشمس: تعدل بين اثنين صدقةٌ، وتعين الرجل في دابته فتحمله عليها أو ترفع له متاعه
-                    صدقةٌ،
-                    والكلمة الطيبة صدقةٌ، وبكل خطوةٍ تمشيها إلى الصلاة صدقةٌ، وتميط الأذى عن الطريق صدقةٌ))؛ رواه
-                    البخاري
-                    ومسلم.
+                <p class="text-[18px] md:text-[22px] pb-4  ">{{ $main_text }}</p>
+                <p class="text-[12px] md:text-[14px] max-w-lg ">
+                   {{ $sub_Text }}
                 </p>
             </div>
             <div class="lg:ml-24">
-                <img src="{{ asset('assets/image/Logo_footer.svg') }}" alt="Logo_footer" class="w-40 h-40 " />
+                <img src="{{ asset($logo_Banner_Img) }}" alt="Logo_footer" class="w-40 h-40 " />
                 <ul class=" flex flex-start items-center gap-x-1 justify-center ">
                     <li>
                         <a class="facebook" href="#">
--- resources/views/Components/Home/Our_business_sectors.blade.php
+++ resources/views/Components/Home/Our_business_sectors.blade.php
@@ -5,43 +5,57 @@
     }
 </style>
 
+
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8 xl:mt-24">
     <div class="flex flex-row flex-wrap gap-y-8 md:gap-y-0 justify-center items-center gap-x-6">
-        <img src="{{ asset('assets/image/emar_deen.svg') }}" alt="emar_deen">
-        <img src="{{ asset('assets/image/somood.svg') }}" alt="somood">
+        @foreach ($partners as $partner)
+        <img src="{{ asset($partner['data']['image']) }}" alt="emar_deen">
+        @endforeach
+        {{-- <img src="{{ asset('assets/image/somood.svg') }}" alt="somood">
         <img src="{{ asset('assets/image/jsoor.svg') }}" alt="jsoor">
         <img src="{{ asset('assets/image/oloom&sharea.svg') }}" alt="oloom&sharea">
-        <img src="{{ asset('assets/image/harkehesalmey.svg') }}" alt="harkehesalmey">
+        <img src="{{ asset('assets/image/harkehesalmey.svg') }}" alt="harkehesalmey"> --}}
     </div>
     <div class="flex flex-col items-start justify-start mt-24">
         <div class="relative ">
-            <p class="text-2xl text-[#101426] font-FlatBold">قطاعات عملنا المركزية</p>
+            @php
+            $text_main_workplace = nova_get_setting('text_main_workplace', 'default_value');
+            $sub_text_workplace = nova_get_setting('sup_text_workplace', 'default_value');
+            @endphp
+            <p class="text-2xl text-[#101426] font-FlatBold">{{ $text_main_workplace }}</p>
             <div class="absolute border-b-[4px] pt-2 border-b-[#349A37] w-9 hidden xl:block"></div>
         </div>
 
-        <p class="pt-7 text-lg text-[#101426] font-noto_Regular max-w-3xl">هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى
-            المقروء لصفحة ما سيلهي القارئ
-            عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. </p>
+        <p class="pt-7 text-lg text-[#101426] font-noto_Regular max-w-3xl">
+            {{ $sub_text_workplace }}
+        </p>
     </div>
     <div class="mt-12">
         <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             <!-- first card -->
+            @if(!empty($news))
+            @foreach ($sectors as $sector)
+            @php
+            $img = $sector['data']['main_img_workplace'];
+                // dd($sector['data']['text_bottom_workplace_']);
+            @endphp
             <div class="pt-6 shaddow relative pb-[72px] overflow-hidden rounded-[16px]">
                 <div class="flow-root rounded-lg bg-[#FFFFFF] px-6 pb-8 ">
                     <div class="-mt-6">
-                        <img src="{{ asset('assets/image/Brand_icon.svg') }}" alt="Brand_icon" class="w-16 h-16">
-                        <h3 class="mt-8 text-lg font-FlatBold text-[#101426]">قطاع الاغاثة</h3>
-                        <p class="mt-4 text-[16px] font-noto_Regular text-[#6B7280]">تقوم الجمعية على تقديم الدعم
-                            الغذائي والصحي لكافة
-                            الشرائح المحتاجة من الداخل والقدس من خلال مجموعة كبيرة من المشاريع الموسمية مثل: افطار
-                            الصائمين ومشروع الأضاحي ومشروع الطرود الغذائية في الأقصى</p>
+                        <img src="{{ asset($img) }}" alt="Brand_icon" class="w-16 h-16">
+                        <h3 class="mt-8 text-lg font-FlatBold text-[#101426]">{{ $sector['data']['text_main_workplace'] }}</h3>
+                        <p class="mt-4 text-[16px] font-noto_Regular text-[#6B7280]">
+                        {{ $sector['data']['sup_text_workplace'] }}
+                        </p>
                     </div>
                 </div>
                 <div
                     class="h-[72px] absolute bottom-0 right-0 left-0 w-full flex flex-row justify-between items-center px-6 cursor-pointer bg-[#F9FAFB]">
-                    <a class="reltive link-wrap font-FlatBold text-[#349A37] text-base h-full w-full flex flex-row justify-between items-center" href="#">
-                        <span class="relative z-10">عرض المزيد</span>
-                        <svg class="relative z-10" width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <a class="reltive link-wrap font-FlatBold text-[#349A37] text-base h-full w-full flex flex-row justify-between items-center"
+                        href="#">
+                        <span class="relative z-10">{{ $sector['data']['text_bottom_workplace_'] }}</span>
+                        <svg class="relative z-10" width="11" height="17" viewBox="0 0 11 17" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M0.654768 8.49995C0.654769 8.19528 0.7711 7.89064 1.00327 7.65835L8.31296 0.34874C8.77795 -0.116247 9.53185 -0.116247 9.99665 0.34874C10.4614 0.813539 10.4614 1.56729 9.99665 2.03231L3.52863 8.49995L9.99642 14.9676C10.4612 15.4326 10.4612 16.1863 9.99642 16.651C9.53162 17.1163 8.77772 17.1163 8.31274 16.651L1.00305 9.34155C0.770836 9.10915 0.654768 8.80451 0.654768 8.49995Z"
                                 fill="#349A37" />
@@ -49,8 +63,11 @@
                     </a>
                 </div>
             </div>
+            @endforeach
+            @endif
+
             <!-- second card -->
-            <div class="pt-6 shaddow relative pb-[72px] overflow-hidden rounded-[16px]">
+            {{-- <div class="pt-6 shaddow relative pb-[72px] overflow-hidden rounded-[16px]">
                 <div class="flow-root rounded-lg bg-[#FFFFFF] px-6 pb-8 ">
                     <div class="-mt-6">
                         <img src="{{ asset('assets/image/finance_Brand.svg') }}" alt="finance_Brand" class="w-16 h-16">
@@ -62,18 +79,20 @@
                 </div>
                 <div
                     class="h-[72px] absolute bottom-0 right-0 left-0 w-full flex flex-row justify-between items-center w-full px-6 cursor-pointer bg-[#F9FAFB]">
-                    <a class="reltive link-wrap font-FlatBold text-[#349A37] text-base h-full w-full flex flex-row justify-between items-center" href="#">
+                    <a class="reltive link-wrap font-FlatBold text-[#349A37] text-base h-full w-full flex flex-row justify-between items-center"
+                        href="#">
                         <span class="relative z-10">عرض المزيد</span>
-                        <svg class="relative z-10" width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="relative z-10" width="11" height="17" viewBox="0 0 11 17" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M0.654768 8.49995C0.654769 8.19528 0.7711 7.89064 1.00327 7.65835L8.31296 0.34874C8.77795 -0.116247 9.53185 -0.116247 9.99665 0.34874C10.4614 0.813539 10.4614 1.56729 9.99665 2.03231L3.52863 8.49995L9.99642 14.9676C10.4612 15.4326 10.4612 16.1863 9.99642 16.651C9.53162 17.1163 8.77772 17.1163 8.31274 16.651L1.00305 9.34155C0.770836 9.10915 0.654768 8.80451 0.654768 8.49995Z"
                                 fill="#349A37" />
                         </svg>
                     </a>
                 </div>
-            </div>
+            </div> --}}
             <!-- third card -->
-            <div class="pt-6 shaddow relative pb-[72px] overflow-hidden rounded-[16px]">
+            {{-- <div class="pt-6 shaddow relative pb-[72px] overflow-hidden rounded-[16px]">
                 <div class="flow-root rounded-lg bg-[#FFFFFF] px-6 pb-8 ">
                     <div class="-mt-6">
                         <img src="{{ asset('assets/image/Brand_icon.svg') }}" alt="Brand_icon" class="w-16 h-16">
@@ -87,16 +106,18 @@
                 </div>
                 <div
                     class="h-[72px] absolute bottom-0 right-0 left-0 w-full flex flex-row justify-between items-center w-full px-6 cursor-pointer bg-[#F9FAFB]">
-                    <a class="reltive link-wrap font-FlatBold text-[#349A37] text-base h-full w-full flex flex-row justify-between items-center" href="#">
+                    <a class="reltive link-wrap font-FlatBold text-[#349A37] text-base h-full w-full flex flex-row justify-between items-center"
+                        href="#">
                         <span class="relative z-10">عرض المزيد</span>
-                        <svg class="relative z-10" width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="relative z-10" width="11" height="17" viewBox="0 0 11 17" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M0.654768 8.49995C0.654769 8.19528 0.7711 7.89064 1.00327 7.65835L8.31296 0.34874C8.77795 -0.116247 9.53185 -0.116247 9.99665 0.34874C10.4614 0.813539 10.4614 1.56729 9.99665 2.03231L3.52863 8.49995L9.99642 14.9676C10.4612 15.4326 10.4612 16.1863 9.99642 16.651C9.53162 17.1163 8.77772 17.1163 8.31274 16.651L1.00305 9.34155C0.770836 9.10915 0.654768 8.80451 0.654768 8.49995Z"
                                 fill="#349A37" />
                         </svg>
                     </a>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
