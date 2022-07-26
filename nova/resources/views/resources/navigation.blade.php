fcfea2e39a4b88ac98b37a68441b8ed5a2a4105a
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2022-07-26 12:25:12 +0300

    push

--- nova/resources/views/resources/navigation.blade.php
+++ nova/resources/views/resources/navigation.blade.php
@@ -1,6 +1,6 @@
 @if (count(\Laravel\Nova\Nova::resourcesForNavigation(request())))
-    <h3 class="flex items-center font-normal text-white mb-6 text-base no-underline">
-        <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+    <h3 class="flex items-center font-normal  mb-6 text-base no-underline">
+        <svg class="sidebar-icon mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path fill="var(--sidebar-icon)" d="M3 1h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3h-4zM3 11h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4h-4z"
             />
         </svg>
@@ -8,15 +8,15 @@
     </h3>
 
     @foreach($navigation as $group => $resources)
-    <details>
+    <details class="mr-2">
 
         @if (count($groups) > 1)
 
-            <summary class="ml-8 mb-4 text-sm text-white uppercase tracking-wide">{{ $group }}</summary>
+            <summary class="ml-8 mb-4 text-sm uppercase tracking-wide">{{ $group }}</summary>
 
     @endif
 
-        <ul class="list-reset mb-8">
+        <ul class="list-reset mb-8 mr-3">
             @foreach($resources as $resource)
                 <li class="leading-tight mb-4 ml-8 text-sm">
                     <router-link :to="{
@@ -24,7 +24,7 @@
                         params: {
                             resourceName: '{{ $resource::uriKey() }}'
                         }
-                    }" class="text-white text-justify no-underline dim" dusk="{{ $resource::uriKey() }}-resource-link">
+                    }" class=" text-justify no-underline dim" dusk="{{ $resource::uriKey() }}-resource-link">
                         {{ $resource::label() }}
                     </router-link>
                 </li>
