399dbef30494b381c682b9ce705694516c930934
ameedasmah1 <ameed.a@averotech.com>
2022-08-03 14:35:32 +0300

    Clean Donations Page

--- resources/views/Pages/project-donations.blade.php
+++ resources/views/Pages/project-donations.blade.php
@@ -17,39 +17,6 @@
         </ul>
     </div>
     @include('Components.ProjectDonations.DonationSteps')
-@include('Components.ProjectDonations.ProjectDonationsDetalis')
-
-
-    {{-- <div class="mt-10 sm:mt-20 flex flex-col gap-y-6">
-        <div class="flex flex-col gap-y-4 md:gap-y-0 md:flex-row items-center justify-start w-full gap-x-5">
-            <input type="text" name="" placeholder=" الاسم الاول"
-                class="rtl block w-[80%] md:w-[50%] border-[#A2A6B0] border rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-4">
-            <input type="text" name="" placeholder=" الاسم الاخير"
-                class="rtl block w-[80%] md:w-[50%] border-[#A2A6B0] border rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-4">
-        </div>
-        <div class="flex flex-col gap-y-4 md:gap-y-0 md:flex-row items-center justify-start w-full gap-x-5">
-            <input type="text" name="" placeholder="رقم الهاتف"
-                class="rtl block w-[80%] md:w-[50%] border-[#A2A6B0] border rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-4">
-            <input type="text" name="" placeholder="رقم البطاقة"
-                class="rtl block w-[80%] md:w-[50%] border-[#A2A6B0] border rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-4">
-        </div>
-        <div class="flex flex-col gap-y-4 md:gap-y-0 md:flex-row items-center justify-start w-full gap-x-5">
-            <input type="text" name="" placeholder="CVV"
-                class="rtl block w-[80%] md:w-[50%] border-[#A2A6B0] border rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-4">
-            <div dir="ltr" class="w-[80%] md:w-[50%]">
-                <input datepicker type="text"
-                    class="block w-full border-[#A2A6B0] border rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-4 text-right"
-                    placeholder="MM/YY">
-            </div>
-        </div>
-        <div class="flex flex-col gap-y-4 md:gap-y-0 md:flex-row items-center justify-start w-full gap-x-5">
-            <img src="assets/image/visa-images.png" alt="cards images">
-        </div>
-        <div class="flex flex-col gap-y-4 md:gap-y-0 md:flex-row items-center justify-start w-full gap-x-5">
-            <button
-                class="rounded-[50px] bg-[#349A37] text-white w-[150px] py-4 font-[700] hover:bg-[#101426] duration-200">تبرع
-                الان</button>
-        </div>
-    </div> --}}
+    @include('Components.ProjectDonations.ProjectDonationsDetalis')
 </div>
 @endsection
