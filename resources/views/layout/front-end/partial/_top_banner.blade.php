abfddc20274a37bdd3bcba142397598b60bdf7c1
ameedasmah1 <ameed.a@averotech.com>
2022-08-14 11:19:32 +0300

    try the banner line now

--- resources/views/layout/front-end/partial/_top_banner.blade.php
+++ resources/views/layout/front-end/partial/_top_banner.blade.php
@@ -30,7 +30,7 @@
                     class="Iman w-full h-full  object-fill rounded-[5px]">
                 <div class="writing sm:block absolute hidden  lg:top-[68%] xl:top-[72%] sm:top-[70%] top-[72%]   right-6 left-6 ">
                     <p itemprop="title" class="text-white bg-[#349A37] text-right font-bold text-sm sm:text-lg pl-2 mx-4 rounded-md px-1 ">
-                        {{Illuminate\Support\Str::limit($lastnew->title,48) }}
+                        {{Illuminate\Support\Str::limit($lastnew->title,46) }}
                         <br/>
                         <span itemprop="description" class="text-white"> {{Illuminate\Support\Str::limit($lastnew->description,45) }}</span>
                     </p>
