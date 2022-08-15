301f32695fa59fe943e8a99e175f763887329bae
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2022-08-15 12:19:09 +0300

    push

---
+++ app/Policies/QawafilAlaqsaPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\QawafilAlaqsa;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class QawafilAlaqsaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->type() == 'admin';
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\QawafilAlaqsa  $qawafilAlaqsa
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, QawafilAlaqsa $qawafilAlaqsa)
+    {
+        return $user->type() == 'admin';
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->type() == 'admin';
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\QawafilAlaqsa  $qawafilAlaqsa
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, QawafilAlaqsa $qawafilAlaqsa)
+    {
+        return $user->type() == 'admin';
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\QawafilAlaqsa  $qawafilAlaqsa
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, QawafilAlaqsa $qawafilAlaqsa)
+    {
+        return $user->type() == 'admin';
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\QawafilAlaqsa  $qawafilAlaqsa
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, QawafilAlaqsa $qawafilAlaqsa)
+    {
+        return $user->type() == 'admin';
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\QawafilAlaqsa  $qawafilAlaqsa
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, QawafilAlaqsa $qawafilAlaqsa)
+    {
+        return $user->type() == 'admin';
+    }
+}
