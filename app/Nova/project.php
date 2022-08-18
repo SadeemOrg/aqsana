b1a5cdcc24c122ef6cca42d806d8342ad51aa57c
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2022-08-18 13:47:40 +0300

    push

--- app/Nova/project.php
+++ app/Nova/project.php
@@ -94,7 +94,7 @@ class Project extends Resource
     {
         $user = Auth::user();
         $id = Auth::id();
-        if ($user->type() == 'admin' || $user->type() == 'website_admin') {
+        if ($user->type() == 'admin' ) {
             return $query;
         } elseif ($user->type() == 'regular_area') {
 
@@ -118,11 +118,11 @@ class Project extends Resource
         return [
             (new Panel(__('main'), [
                 ID::make(__('ID'), 'id')->sortable(),
-                ActionButton::make('Action')
-                ->action(ApprovalRejectProjec::class, $this->id)
-                ->hideWhenCreating()->hideWhenUpdating(),
-                Text::make("project name", "project_name"),
-                Text::make("project describe", "project_describe"),
+                ActionButton::make(__('Action'))
+                    ->action(ApprovalRejectProjec::class, $this->id)
+                    ->hideWhenCreating()->hideWhenUpdating(),
+                Text::make(__("project name"), "project_name"),
+                Text::make(__("project describe"), "project_describe"),
                 Select::make(__('SECTOR'), 'sector')
                     ->options(function () {
                         $sectors = nova_get_setting('workplace', 'default_value');
@@ -134,7 +134,7 @@ class Project extends Resource
                             return  $user_type_admin_array;
                         }
                     }),
-                BelongsToManyField::make('Area', 'Area')
+                BelongsToManyField::make(__('Area'),"Area" ,'\App\Nova\Area')
                     ->options(Area::all())
                     ->optionsLabel('name')->canSee(function ($request) {
                         $user = Auth::user();
@@ -143,8 +143,8 @@ class Project extends Resource
                     }),
 
 
-                DateTime::make('projec start', 'start_date'),
-                DateTime::make('projec end', 'end_date'),
+                DateTime::make(__('projec start'), 'start_date'),
+                DateTime::make(__('projec end'), 'end_date'),
 
 
                 Boolean::make(__('is_bus'), 'is_bus'),
@@ -152,15 +152,15 @@ class Project extends Resource
                 Boolean::make(__('is_has_Donations'), 'is_donation'),
 
                 Select::make(__('is_reported'), 'is_reported')->options([
-                    '1' => 'yes',
-                    '0' => 'no',
+                    '1' => 'نعم',
+                    '0' => 'لا',
                 ])->displayUsingLabels(),
 
 
                 NovaDependencyContainer::make([
-                    Text::make("Title", 'report_title'),
-                    Textarea::make('description', 'report_description'),
-                    Tiptap::make('Contents', 'report_contents')
+                    Text::make(__("Title"), 'report_title'),
+                    Textarea::make(__('description'), 'report_description'),
+                    Tiptap::make(__('Contents'), 'report_contents')
                         ->buttons([
                             'heading',
                             '|',
@@ -195,10 +195,10 @@ class Project extends Resource
                         ->headingLevels([1, 2, 3, 4, 5, 6]),
 
 
-                    Image::make('Image', 'report_image')->disk('public')->prunable(),
-                    ArrayImages::make('Pictures', 'report_pictures')
+                    Image::make(__('Image'), 'report_image')->disk('public')->prunable(),
+                    ArrayImages::make(__('Pictures'), 'report_pictures')
                         ->disk('public'),
-                    Text::make("video link", 'report_video_link'),
+                    Text::make(__("video link"), 'report_video_link'),
                     Date::make(__('DATE'), 'report_date')->pickerDisplayFormat('d.m.Y'),
 
                 ])->dependsOn('is_reported', '1'),
@@ -236,7 +236,7 @@ class Project extends Resource
                         return false;
                     }),
             ])),
-            (new Panel(__('Budjet'), [
+            (new Panel(__('Budget'), [
 
                 Text::make("Budjet", "Budjet", function () {
 
@@ -264,7 +264,7 @@ class Project extends Resource
                     return null;
                 }),
             ])),
-            (new Panel(__('Toole'), [
+            (new Panel(__('tooles'), [
 
                 Text::make("Toole", "Toole", function () {
 
@@ -472,12 +472,38 @@ class Project extends Resource
     }
     public static function beforeCreate(Request $request, $model)
     {
-        $id = Auth::id();
+                $id = Auth::id();
         $model->created_by = $id;
+    }
 
+    public static function afterCreate(Request $request, $model)
+    {
+        $user = Auth::user();
+        $id = Auth::id();
+        $citye =   City::where('admin_id', $id)
+            ->first();
+        if ($user->type() == 'regular_area') {
+            $Area = \App\Models\Area::where('admin_id', $id)->first();
+            DB::table('project_area')
+                ->updateOrInsert(
+                    ['project_id' => $model->id, 'area_id' =>  $Area['id']],
 
+                );
+        }
+        if ($user->type() == 'regular_city') {
+            // dd($citye);
+            DB::table('project_area')
+            ->updateOrInsert(
+                ['project_id' => $model->id, 'area_id' =>  $citye['area_id']],
 
-        // dd(gettype($Area));
+            );
+            DB::table('project_city')
+                ->updateOrInsert(
+                    ['project_id' => $model->id, 'city_id' =>  $citye['id']],
+
+                );
+        }
+
     }
     public static function beforeUpdate(Request $request, $model)
     {
