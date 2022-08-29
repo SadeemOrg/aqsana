00748bda397a940042a00ba4430e0e69310ca45e
ameedasmah1 <ameed.a@averotech.com>
2022-08-29 14:52:55 +0300

    adding data

--- resources/views/Pages/projectDonations/project-donation-details.blade.php
+++ resources/views/Pages/projectDonations/project-donation-details.blade.php
@@ -128,7 +128,8 @@ $imageVideoCover = "https://c.ndtvimg.com/2022-04/e0ei6018_jerusalem-al-aqsa-mos
                             </a>
                         </li>
                         <li class="px-1">
-                            <a class="pinterest"  target="_self"
+                            <a class="pinterest"
+                            target="_blank"
                              {{--href="https://pinterest.com/pin/create/button/?url={{ Request::url() }}&media={{ $image }}&description={{ $project->title }}"
                                 --}}>
                                 <svg width="25" height="25" viewBox="0 0 25 25" fill="none"
@@ -141,7 +142,7 @@ $imageVideoCover = "https://c.ndtvimg.com/2022-04/e0ei6018_jerusalem-al-aqsa-mos
                             </a>
                         </li>
                         <li class="px-1">
-                            <a class="linkedin" target="_self" href="https://www.linkedin.com/sharing/share-offsite/?url={url}">
+                            <a class="linkedin" target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url={url}">
                                 <svg width="25" height="25" viewBox="0 0 25 25" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="12.1687" cy="12.1687" r="11.6687" stroke="#101426" />
