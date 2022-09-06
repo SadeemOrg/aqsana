591a106251a01445879a27dc640702421966de4b
ameedasmah1 <ameed.a@averotech.com>
2022-09-06 13:10:51 +0300

    Modify Header

--- resources/views/layout/front-end/partial/navbar-item.blade.php
+++ resources/views/layout/front-end/partial/navbar-item.blade.php
@@ -1,24 +1,15 @@
-{{-- @php
-    $hh='fffsguisdgyskdanckjsdnflkjes   ';
-@endphp
-<div   onclick="add('{{  $hh }}');">fi esnhkjfnaeakfjnsejfnse</div>
-<script>
- function add(id)
-{
-    alert(id);
-}
 
-</script> --}}
+@php
+$img = 'storage/' . nova_get_setting('logo', 'default_value');
+@endphp
 @if (empty($item->children))
-<li class="nav-item relative">
+<li class="nav-item relative ">
     <a class=" w-[250px] xl:w-auto mb-3 xl:mb-0 xl:hover:text-[#349A37] bg-[#349A37] xl:bg-transparent text-white xl:text-[#000] block py-3 px-4 xl:py-0 xl:px-0  xl:inline-block relative xl:static"
         href="/{{ $item->data->link->id }}" target="_self">{{$item->data->name}}</a>
 </li>
 @else
 <li class="nav-item relative">
-
     @if ( $item->data->link->resource=='external')
-
     <a class=" w-[250px] xl:w-auto mb-3 xl:mb-0 xl:hover:text-[#349A37] bg-[#349A37] xl:bg-transparent text-white xl:text-[#000] block py-3 px-4 xl:py-0 xl:px-0  xl:inline-block relative xl:static"
     href="/{{ $item->data->link->id }}" target="_self">{{$item->data->name}}</a>
     @else
