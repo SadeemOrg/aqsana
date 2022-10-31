67fda1744bcd421e2a4c11f421fc2e1ee48a130c
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2022-10-31 14:05:59 +0200

    push

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Budget extends Model
 {
     use HasFactory;
+    public function Sector()
+    {
+        return $this->hasOne(Sector::class,'id','sector_id');
+    }
 }
