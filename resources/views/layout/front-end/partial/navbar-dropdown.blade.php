3df1d3d04e9efe500633dc558b0674ac46154c1e
ameedasmah1 <ameed.a@averotech.com>
2022-11-16 11:28:47 +0200

    Adding Alhasad

--- resources/views/layout/front-end/partial/navbar-dropdown.blade.php
+++ resources/views/layout/front-end/partial/navbar-dropdown.blade.php
@@ -7,6 +7,7 @@
                     href="{{ route('getprojectDetail', ['id' => $item->data->link->id]) }}"
                     target="_self">{{ $item->data->name }}</a>
             @else
+            
                 @if ($itemName == 'الحصاد')
                 <a class="py-2 border-b border-[#CCDDFF] block hover:bg-[#349A37] hover:text-white px-3 duration-200"
                 href="{{ $item->data->link->id }}" target="_blank">{{ $item->data->name }}</a>
