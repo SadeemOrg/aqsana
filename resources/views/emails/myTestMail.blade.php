9eb6bf57c5f23fb73c0d6449feba82c4413511b3
ameedasmah1 <ameed.a@averotech.com>
2023-01-11 19:53:31 +0200

    Modify Arabic Bills

--- resources/views/emails/myTestMail.blade.php
+++ resources/views/emails/myTestMail.blade.php
@@ -9,13 +9,13 @@
     @endphp
     <h1>Bill  </h1>
 
-    <p><span>sector </span> {{ $Transaction->Sectors->text }}</p>
-    <p><span>ref_id </span> {{ $Transaction->Project->project_name }}</p>
-
+    {{-- <p><span>sector </span> {{ $Transaction->Sectors->text }}</p>
+    <p><span>ref_id </span> {{ $Transaction->Project->project_name }}</p> --}}
+{{-- 
       <p><span>equivelant_amount </span> {{ $Transaction->equivelant_amount }}</p>
       <p><span>name </span> {{ $Transaction->TelephoneDirectory->name }}</p>
       <p><span>company_number </span> {{ $Transaction->company_number }}</p>
-      <p><span>bill_number </span> {{ $Transaction->bill_number }}</p>
+      <p><span>bill_number </span> {{ $Transaction->bill_number }}</p> --}}
     <p>Thank you</p>
 </body>
 </html>
