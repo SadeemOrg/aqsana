52d28381fc55f3b47756e5ec8af2655a4dac83f5
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-02-09 14:22:42 +0200

    modify Bills

--- resources/views/Pages/Bills/HebrowBills.blade.php
+++ resources/views/Pages/Bills/HebrowBills.blade.php
@@ -31,8 +31,8 @@
                     <span class="font-FlatBold text-[#6B7280] mx-1 text-sm">{{ $email }}</span>
                 </p>
             </div>
-            <div class="sm:hidden flex flex-col items-center justify-center w-full my-4 gap-y-2 px-14">
-                <div class=" flex flex-row flex-wrap items-start w-full justify-around gap-x-2">
+            <div class="sm:hidden flex flex-col items-center justify-center w-full my-4 gap-y-2 px-4">
+                <div class=" flex flex-row flex-wrap items-start w-full justify-around gap-x-6">
                     <p class=" mt-3 text-[17px] font-noto_Regular text-[#101426]">אִרגוּן :
                         <span class="font-FlatBold text-[#6B7280] mx-1 text-sm">{{ $society_id }}</span>
                     </p>
@@ -40,7 +40,7 @@
                         <span class="font-FlatBold text-[#6B7280] mx-1 ">{{ $newaddress[0] }}</span>
                     </p>
                 </div>
-                <div class=" flex flex-row flex-wrap items-start w-full justify-around gap-x-2">
+                <div class=" flex flex-row flex-wrap items-start w-full justify-around gap-x-6">
                     <p class=" text-[17px] font-noto_Regular text-[#101426]">טלפון :
                         <span dir="ltr" class="font-FlatBold text-[#6B7280] mx-1 text-sm">{{ $phone }}</span>
                     </p>
