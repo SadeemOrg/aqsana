2456950ff49fdfd58a4ee5ba87b038a8d32602ee
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-02-26 15:39:20 +0200

    push

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,14 @@
  .vue()
  .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
 
-
+ /*
  mix.copy('node_modules/push.js/bin/push.min.js',
     'public/assets/js/push.min.js');
+    const mix = require('laravel-mix');
+
+
+
+    mix.js('resources/js/app.js', 'public/js')
+        .sass('resources/sass/app.scss', 'public/css')
+        .sourceMaps();
+        */
