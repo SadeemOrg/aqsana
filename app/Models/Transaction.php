ae2c43794ab541a803362774cf3fd7fc19f8c952
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-02-28 11:03:42 +0200

    push

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Whitecube\NovaFlexibleContent\Value\FlexibleCast;
-
-class Transaction extends Model
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+class Transaction extends Model implements HasMedia
 {
-
+    use InteractsWithMedia;
     use HasFactory;
 
     protected $fillable = [
@@ -34,6 +36,19 @@ class Transaction extends Model
         'transaction_date' => 'date',
         'Date' => 'date',
     ];
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('file')
+            ->width(130)
+            ->height(130);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('main')->singleFile();
+        $this->addMediaCollection('my_multi_collection');
+    }
 
     public function Trip()
     {
