f73e02b71138001ac22e07cbd436912d2318a940
Nserallah <moslem.12166@gmail.com>
2023-08-03 16:18:07 +0300

    solve problems with mobile in dashboard sidebar

--- resources/views/vendor/nova/layout.blade.php
+++ resources/views/vendor/nova/layout.blade.php
@@ -298,47 +298,60 @@
         Nova.liftOff()
     </script>
     <script>
-        (function() {
-
-            var dropBtns = document.querySelectorAll('.dropdown');
-
-            function closeOpenItems() {
-                openMenus = document.querySelectorAll('.dropMenu');
-                openMenus.forEach(function(menus) {
-                    menus.classList.remove('show');
-                });
-            }
-
-            dropBtns.forEach(function(btn) {
-
-                btn.addEventListener('click', function(e) {
-                    var
-                        dropContent = btn.querySelector('.dropMenu'),
-                        shouldOpen = !dropContent.classList.contains('show');
-                    e.preventDefault();
-
-                    // First close all open items.
-                    closeOpenItems();
-                    // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
-                    if (shouldOpen) {
-                        // Open the clicked item.
-                        dropContent.classList.add('show');
-                    }
-                    e.stopPropagation();
-                });
-
-
+        $( document ).ready(function() {
+            $('.main-items').click(function() {
+                $('.w-sidebar').removeClass('sidebar-hidden');
+                $('.main-items').next('.room-sort').slideToggle();
+            })
+            $('.dropdown .dropBtn, .dropdown').click(function(event) {
+                event.preventDefault();
+                $('.w-sidebar').removeClass('sidebar-hidden');
+                $(this).next('.dropMenu').slideToggle();
             });
+        });
 
-            //   close menus when clicking outside of them
-            window.addEventListener('click', function(event) {
-                if (event.target != dropBtns) {
-                    // Moved the code here to its own function.
-                    closeOpenItems();
-                }
-            });
 
-        })();
+        // (function() {
+
+        //     var dropBtns = document.querySelectorAll('.dropdown');
+
+        //     function closeOpenItems() {
+        //         openMenus = document.querySelectorAll('.dropMenu');
+        //         openMenus.forEach(function(menus) {
+        //             menus.classList.remove('show');
+        //         });
+        //     }
+
+        //     dropBtns.forEach(function(btn) {
+
+        //         btn.addEventListener('click', function(e) {
+        //             var
+        //                 dropContent = btn.querySelector('.dropMenu'),
+        //                 shouldOpen = !dropContent.classList.contains('show');
+        //             e.preventDefault();
+
+        //             // First close all open items.
+        //             closeOpenItems();
+        //             // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
+        //             if (shouldOpen) {
+        //                 // Open the clicked item.
+        //                 dropContent.classList.add('show');
+        //             }
+        //             e.stopPropagation();
+        //         });
+
+
+        //     });
+
+        //     //   close menus when clicking outside of them
+        //     window.addEventListener('click', function(event) {
+        //         if (event.target != dropBtns) {
+        //             // Moved the code here to its own function.
+        //             closeOpenItems();
+        //         }
+        //     });
+
+        // })();
     </script>
     {{-- <script>
         function loadlink() {
