f0b46279a5da0ddf0e2016e4e99690534a8cd41d
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-09-24 11:17:54 +0300

    add

--- resources/views/livewire/export-donations.blade.php
+++ resources/views/livewire/export-donations.blade.php
@@ -2,21 +2,21 @@
     @if ($this->key=='Donations')
 
 
-    <button id='clickButton' wire:click="Donations" >Click Me</button>
+    <button id='clickButton' wire:click="Donations"  hiddenhidden>Click Me</button>
    @elseif ($this->key=='PaymentVoucher')
-   <button id='clickButton' wire:click="PaymentVoucher" >Click Me</button>
+   <button id='clickButton' wire:click="PaymentVoucher" hidden>Click Me</button>
    @elseif ($this->key=='Address')
-   <button id='clickButton' wire:click="Address" >Click Me</button>
+   <button id='clickButton' wire:click="Address" hidden>Click Me</button>
    @elseif ($this->key=='Alhisalat')
-   <button id='clickButton' wire:click="Alhisalat" >Click Me</button>
+   <button id='clickButton' wire:click="Alhisalat" hidden>Click Me</button>
    @elseif ($this->key=='Area')
-   <button id='clickButton' wire:click="Area" >Click Me</button>
+   <button id='clickButton' wire:click="Area" hidden>Click Me</button>
    @elseif ($this->key=='Users')
-   <button id='clickButton' wire:click="Users" >Click Me</button>
+   <button id='clickButton' wire:click="Users" hidden>Click Me</button>
    @elseif ($this->key=='Cites')
-   <button id='clickButton' wire:click="Cites" >Click Me</button>
+   <button id='clickButton' wire:click="Cites" hidden>Click Me</button>
    @elseif ($this->key=='BusesCompany')
-   <button id='clickButton' wire:click="BusesCompany" >Click Me</button>
+   <button id='clickButton' wire:click="BusesCompany" hidden>Click Me</button>
 
     @endif
     <script>
