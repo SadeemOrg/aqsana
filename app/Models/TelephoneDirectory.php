ab71fd72d53f803e6c529e3509c751048f2550d2
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-10-05 11:13:21 +0300

    add

--- app/Models/TelephoneDirectory.php
+++ app/Models/TelephoneDirectory.php
@@ -5,41 +5,42 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Whitecube\NovaFlexibleContent\Value\FlexibleCast;
+
 class TelephoneDirectory extends Model
 {
 
     use HasFactory;
 
     protected $fillable = [
-    'name','email','type','phone_number','city','roles','job','id_number','Area','birth_date'
-   ];
+        'name', 'email', 'type', 'phone_number', 'city', 'roles', 'job', 'id_number', 'Area', 'birth_date'
+    ];
 
-   protected $casts = [
-    // 'hower' => FlexibleCast::class,
-    'hower' => 'json',
-    'birth_date' => 'date',
- 'type' => 'json',
-
-
-];
+    protected $casts = [
+        // 'hower' => FlexibleCast::class,
+        'hower' => 'json',
+        'birth_date' => 'date',
+        'type' => 'json',
 
 
-public function AreaDelegate()
-{
-    return $this->belongsTo(Area::class,'Area');
-}
+    ];
 
-public function citeDelegate()
-{
-    return $this->belongsTo(City::class,'city');
-}
-public function SmsType()
-{
-    return $this->hasMany(SmsType::class,"type");
-}
-public function ActionEvents()
-{
-    return $this->hasMany(ActionEvents::class,"actionable_id")->where('action_events.target_type', '=', get_class($this));
-}
 
+    public function AreaDelegate()
+    {
+        return $this->belongsTo(Area::class, 'Area');
+    }
+
+    public function citeDelegate()
+    {
+        return $this->belongsTo(City::class, 'city');
+    }
+    public function SmsType()
+    {
+        return $this->hasMany(SmsType::class, "type");
+    }
+    public function ActionEvents()
+    {
+        // dd(get_class($this));
+        return $this->hasMany(ActionEvents::class, "actionable_id")->where('action_events.target_type', '=', get_class($this));
+    }
 }
