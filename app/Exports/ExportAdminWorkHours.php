42ca7a0c188ec0bb4fd132c90000bc701d6b0bf3
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-11-02 11:24:05 +0200

    Add

--- app/Exports/ExportAdminWorkHours.php
+++ app/Exports/ExportAdminWorkHours.php
@@ -3,13 +3,14 @@
 namespace App\Exports;
 
 use App\Models\WorkHours;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class ExportAdminWorkHours implements FromCollection, WithHeadings
 {
 
-    public function __construct(string $id ,string $startdate ,string $finishdate )
+    public function __construct(string $id, string $startdate, string $finishdate)
     {
         $this->id = $id;
         $this->startdate = $startdate;
@@ -17,26 +18,51 @@ class ExportAdminWorkHours implements FromCollection, WithHeadings
     }
     public function headings(): array
     {
-          $array = [];
+        $array = [];
 
-          array_push($array, 'اليوم');
-          array_push($array, 'التاريخ');
-          array_push($array, 'بداء العمل');
-          array_push($array, 'انتهاء العمل');
-          array_push($array, 'ساعات عمل اليوم');
+        array_push($array, 'اليوم');
+        array_push($array, 'التاريخ');
+        array_push($array, 'بداء العمل');
+        array_push($array, 'انتهاء العمل');
+        array_push($array, 'ساعات عمل اليوم');
 
 
-          return $array;
+        return $array;
     }
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
+
         $from = date($this->startdate);
         $to = date($this->finishdate);
+        $string = '2001-01-01 00:00:00.0';
+        $date = Carbon::parse($string);
+        $WorkHours = WorkHours::whereBetween('date', [$from, $to])->where('user_id', '=', $this->id)
+            ->select('day', 'date', 'start_time', 'end_time', 'day_hours')->orderBy('date', 'ASC')->get();
+        foreach ($WorkHours as $key => $value) {
+            if ($value->day_hours != null) {
+                $time2 = Carbon::parse($value->day_hours);
+                $hours = $time2->hour;
+                $minutes = $time2->minute;
+                $seconds = $time2->second;
 
-        return WorkHours::whereBetween('date', [$from, $to])->where('user_id', '=', $this->id)
-        ->select('day', 'date', 'start_time','end_time','day_hours')->orderBy('date', 'ASC')->get();
+                $date->addSeconds($seconds)->addMinutes($minutes)->addHours($hours);
+            }
+        }
+        $date = Carbon::parse($date);
+        $sumWorkHourssearch = $date;
+        $endHour =  ($sumWorkHourssearch->day - 1) * 24 + $sumWorkHourssearch->hour . ':' . $sumWorkHourssearch->minute . ':' . $sumWorkHourssearch->second;
+
+        $additionalRows = [
+            ['Column1', 'Column2', 'Column3'],
+            ['', 'مجموع الساعات',  $endHour],
+            // Add more rows as needed
+        ];
+        $mergedQuery = $WorkHours->union($additionalRows);
+
+// dd($additionalRows,$mergedQuery);
+        return $mergedQuery;
     }
 }
