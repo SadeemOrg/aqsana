6b10e00da8919d49db63694eaf07117218efb0d1
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2024-05-15 13:35:53 +0300

    add

--- app/Nova/Filters/DateRange.php
+++ app/Nova/Filters/DateRange.php
@@ -15,7 +15,7 @@ class DateRange extends DateFilter
      *
      * @var string
      */
-    public $component = 'searchable-select-filter';
+    // public $component = 'searchable-select-filter';
 
     /**
      * Apply the filter to the given query.
