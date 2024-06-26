7b41eb87d66c9c3d721ed7e255f0dca57233fdc6
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-06-26 14:37:45 +0300

    add trip

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -53,11 +53,11 @@ class TripController extends BaseController
                 $to_lng = -180;
             }
 
-            $from_distance = Helpers::distance($request->lat, $request->lng, $from_lat, $from_lng, 'K');
+            $from_distance = Helpers::distance(32.22186486702175, 35.256424066320406, $from_lat, $from_lng, 'K');
             $trip->from_distance = round($from_distance, 2);
 
 
-            $to_distance = Helpers::distance($request->lat, $request->lng, $to_lat, $to_lng, 'K');
+            $to_distance = Helpers::distance(32.22186486702175, 35.256424066320406, $to_lat, $to_lng, 'K');
             $trip->to_distance = round($to_distance, 2);
 
             if (Auth()->id() != null) {
@@ -134,11 +134,11 @@ class TripController extends BaseController
                 $to_lng = -180;
             }
 
-            $from_distance = Helpers::distance($request->lat, $request->lng, $from_lat, $from_lng, 'K');
+            $from_distance = Helpers::distance(32.22186486702175, 35.256424066320406, $from_lat, $from_lng, 'K');
             $trip->from_distance = round($from_distance, 2);
 
 
-            $to_distance = Helpers::distance($request->lat, $request->lng, $to_lat, $to_lng, 'K');
+            $to_distance = Helpers::distance(32.22186486702175, 35.256424066320406, $to_lat, $to_lng, 'K');
             $trip->to_distance = round($to_distance, 2);
 
             if (Auth()->id() != null) {
@@ -205,11 +205,11 @@ class TripController extends BaseController
             }
 
 
-            $from_distance = Helpers::distance($request->lat, $request->lng, $from_lat, $from_lng, 'K');
+            $from_distance = Helpers::distance(32.22186486702175, 35.256424066320406, $from_lat, $from_lng, 'K');
             $trip->from_distance = round($from_distance, 2);
 
 
-            $to_distance = Helpers::distance($request->lat, $request->lng, $to_lat, $to_lng, 'K');
+            $to_distance = Helpers::distance(32.22186486702175, 35.256424066320406, $to_lat, $to_lng, 'K');
             $trip->to_distance = round($to_distance, 2);
 
 
