82ac2e6315d9725bf3d43b0f2c7819a56320c5a6
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-06-26 15:33:59 +0300

    edit api trip

--- app/CPU/helpers.php
+++ app/CPU/helpers.php
@@ -3,104 +3,108 @@
 namespace App\CPU;
 
 
-class Helpers
-{
+class Helpers {
 
-    public static function send_notification($tokens, $notification)
-    {
+   public static function send_notification($tokens,$notification) {
 
 
-        $server_key = "AAAAA_Hl3RU:APA91bG0Fqxoqxi703Ov637hTwDZx99ezBvlcpETyJOyXod65v2Wp9KVM-Bk_uGAYGyBmTpjbcp_RO9B8Y9P_AhM9K1DuB10zEHriHAFRcmrGrSMIQdKg-Scf05TWgN5ugdwnipdY3mv";
-        $msg = array(
+            $server_key = "AAAAA_Hl3RU:APA91bG0Fqxoqxi703Ov637hTwDZx99ezBvlcpETyJOyXod65v2Wp9KVM-Bk_uGAYGyBmTpjbcp_RO9B8Y9P_AhM9K1DuB10zEHriHAFRcmrGrSMIQdKg-Scf05TWgN5ugdwnipdY3mv";
+            $msg = array
+                  (
+                    'body'  => $notification->message,
+                    'title' => $notification->title,
+                    'receiver' => 'erw',
+                    'icon'  => "http://url-to-an-icon/icon.png",/*Default Icon*/
+                    'sound' => 'mySound'/*Default sound*/
+                  );
+
+            $fields = array
+                    (
+                        'registration_ids' => $tokens,
+                        'notification'  => $msg
+                    );
+
+            $headers = array
+                    (
+                        'Authorization: key=' . $server_key,
+                        'Content-Type: application/json'
+                    );
+            //#Send Reponse To FireBase Server
+            $ch = curl_init();
+            curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+            curl_setopt( $ch,CURLOPT_POST, true );
+            curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+            curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+            curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+            curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+            $result = curl_exec($ch );
+            // dd($result);
+            curl_close( $ch );
+
+
+            return back();
+
+   }
+
+   public static function send_notification_topic($topic,$notification) {
+
+    $server_key = "AAAAA_Hl3RU:APA91bG0Fqxoqxi703Ov637hTwDZx99ezBvlcpETyJOyXod65v2Wp9KVM-Bk_uGAYGyBmTpjbcp_RO9B8Y9P_AhM9K1DuB10zEHriHAFRcmrGrSMIQdKg-Scf05TWgN5ugdwnipdY3mv";
+    $msg = array
+          (
             'body'  => $notification->message,
             'title' => $notification->title,
             'receiver' => 'erw',
             'icon'  => "http://url-to-an-icon/icon.png",/*Default Icon*/
             'sound' => 'mySound'/*Default sound*/
-        );
+          );
 
-        $fields = array(
-            'registration_ids' => $tokens,
-            'notification'  => $msg
-        );
+    $fields = array
+            (
+                'to' => "/topics/$topic",
+                'notification'  => $msg
+            );
 
-        $headers = array(
-            'Authorization: key=' . $server_key,
-            'Content-Type: application/json'
-        );
-        //#Send Reponse To FireBase Server
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-        $result = curl_exec($ch);
-        // dd($result);
-        curl_close($ch);
-
-
-        return back();
-    }
-
-    public static function send_notification_topic($topic, $notification)
-    {
-
-        $server_key = "AAAAA_Hl3RU:APA91bG0Fqxoqxi703Ov637hTwDZx99ezBvlcpETyJOyXod65v2Wp9KVM-Bk_uGAYGyBmTpjbcp_RO9B8Y9P_AhM9K1DuB10zEHriHAFRcmrGrSMIQdKg-Scf05TWgN5ugdwnipdY3mv";
-        $msg = array(
-            'body'  => $notification->message,
-            'title' => $notification->title,
-            'receiver' => 'erw',
-            'icon'  => "http://url-to-an-icon/icon.png",/*Default Icon*/
-            'sound' => 'mySound'/*Default sound*/
-        );
-
-        $fields = array(
-            'to' => "/topics/$topic",
-            'notification'  => $msg
-        );
-
-        $headers = array(
-            'Authorization: key=' . $server_key,
-            'Content-Type: application/json'
-        );
-        //#Send Reponse To FireBase Server
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-        $result = curl_exec($ch);
-        // dd($result);
-        curl_close($ch);
+    $headers = array
+            (
+                'Authorization: key=' . $server_key,
+                'Content-Type: application/json'
+            );
+    //#Send Reponse To FireBase Server
+    $ch = curl_init();
+    curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+    curl_setopt( $ch,CURLOPT_POST, true );
+    curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+    curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+    curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+    $result = curl_exec($ch );
+    // dd($result);
+    curl_close( $ch );
 
 
-        return back();
-    }
+    return back();
 
-    public static function distance($lat1, $lon1, $lat2, $lon2, $unit)
-    {
-        $earth_radius = 6371; // Radius of the earth in km
-
-        $dLat = deg2rad($lat2 - $lat1);
-        $dLon = deg2rad($lon2 - $lon1);
-
-        $a = sin($dLat / 2) * sin($dLat / 2) +
-            cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
-            sin($dLon / 2) * sin($dLon / 2);
-        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-
-        $distance = $earth_radius * $c;
-
-        if ($unit == 'M') {
-            $distance *= 0.621371;
-        } else if ($unit == 'N') {
-            $distance *= 0.539957;
-        }
-
-        return $distance;
-    }
 }
+
+public static function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    $unit = strtoupper($unit);
+
+    if ($unit == "K") {
+        return ($miles * 1.609344);
+    } else if ($unit == "N") {
+        return ($miles * 0.8684);
+    } else {
+        return $miles;
+    }
+  }
+
+}
+
+?>
+
