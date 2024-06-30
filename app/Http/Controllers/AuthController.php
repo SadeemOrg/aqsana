f7cdae08217f2743c129144a7f4d68bce662a5cc
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-06-30 15:01:16 +0300

    edit phone_number  to en if send in ar

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,9 +47,13 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $requestData = $request->all();
+        $arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+        $englishNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+        $requestData['phone_number'] = str_replace($arabicNumbers, $englishNumbers, $requestData['phone_number']);
         $requestData['phone'] = $requestData['phone_number'];
 
-        $validator = Validator::make( $requestData, [
+        $validator = Validator::make($requestData, [
             'name' => 'required|string',
             'email' => 'required|string|unique:users|email',
             'password' => 'required|string',
