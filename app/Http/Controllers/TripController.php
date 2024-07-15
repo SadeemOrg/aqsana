0623153430b07eded93eedcbbef1e8b68aa37ff7
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-07-15 14:13:27 +0300

    edit search_trip

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -208,6 +208,7 @@ class TripController extends BaseController
 
 
         $trips = Project::where("project_type", "2")->with('TripCity.City', 'BusTrip.travelto', 'BusTrip.travelfrom', 'tripfrom', 'tripto')
+                ->where('start_date', '>', Carbon::now())
             ->whereDate('end_date', '>=', date('Y-m-d H:i:s'))->orderBy('created_at', 'desc')->get();
 
         $search_trip = collect();
