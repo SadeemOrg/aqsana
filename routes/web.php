ccb56337f1da30aae4384b23fb43813eb209efe1
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-07-17 15:43:03 +0300

    add type to bill

--- routes/web.php
+++ routes/web.php
@@ -225,8 +225,8 @@ Route::get('/librarysearch/{search}', [HomeController::class, 'librarySearch'])-
 Route::get('/librarySearchType/{id}', [HomeController::class, 'librarySearchType'])->name('librarySearchType');
 
 
-Route::get('originalbill/{id}', [HomeController::class, 'originalbillbills'])->name('originalbill');
-Route::get('bill/{id}', [HomeController::class, 'bills'])->name('bill');
+Route::get('originalbill/{id}/{type?}', [HomeController::class, 'originalbillbills'])->name('originalbill');
+Route::get('bill/{id}/{type?}', [HomeController::class, 'bills'])->name('bill');
 Route::get('/mainbill/{id}', [HomeController::class, 'mainbill'])->name('mainbill');
 Route::get('/SendMail', [HomeController::class, 'SendMail'])->name('SendMail');
 Route::post('/SendMail', [HomeController::class, 'SendMail'])->name('SendMail');
@@ -286,7 +286,7 @@ Route::get("/WorkHoursUser", [HomeController::class, "WorkHoursUser"])->name('Wo
 Route::get("/StartTimerWorkHours", [HomeController::class, "StartTimerWorkHours"])->name('StartTimerWorkHours');
 
 
-Route::get('generate-pdf/{id}', [PDFController::class, 'generatePDF'])->name('generate-pdf');
+Route::get('generate-pdf/{id}/{type?}', [PDFController::class, 'generatePDF'])->name('generate-pdf');
 
 Route::get('generate-pdf-hours', [PDFController::class, 'generatePDFHours'])->name('generate-pdf-hours');
 
