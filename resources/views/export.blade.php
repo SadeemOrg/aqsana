f507402553aee9a4ddcc14d2f2c7769ffe4aeaf7
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-09-04 23:35:58 +0300

    edit export exsel

--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -1,13 +1,16 @@
+@extends('layout.app', ['hasHeader' => false, 'hasFooter' => false])
 
-@extends('layout.app', ['hasHeader' => false, 'hasFooter' => false ])
+
+
 @section('content')
-    @if ($key=='Report')
-
-    @livewire('export-report', ['key' => $key,'ref' => $ref,'name'=>$name,'from'=>$from,'to'=>$to,'dateType'=>$dateType,'PaymentType'=>$PaymentType])
-
+    @if ($key == 'Report')
+        @if ($print == 1)
+            @livewire('export-report', ['key' => $key, 'ref' => $ref, 'name' => $name, 'from' => $from, 'to' => $to, 'dateType' => $dateType, 'PaymentType' => $PaymentType])
+        @else
+            @livewire('export-donations', ['key' => $key, 'ref' => $ref, 'name' => $name, 'from' => $from, 'to' => $to, 'dateType' => $dateType, 'PaymentType' => $PaymentType])
+        @endif
     @else
-
-    @livewire('export-donations', ['key' => $key,'ref' => $ref,'name'=>$name,'from'=>$from,'to'=>$to,'dateType'=>$dateType,'PaymentType'=>$PaymentType])
+        @livewire('export-donations', ['key' => $key, 'ref' => $ref, 'name' => $name, 'from' => $from, 'to' => $to, 'dateType' => $dateType, 'PaymentType' => $PaymentType])
     @endif
 
 
