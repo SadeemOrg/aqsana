f9399256af48fe4c981418abaa73f4eaf92d3210
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-10-16 11:39:41 +0300

    edit popModel

--- resources/views/layout/front-end/partial/_popModal.blade.php
+++ resources/views/layout/front-end/partial/_popModal.blade.php
@@ -18,21 +18,20 @@
             <div
                 class="relative ModalContainer bg-white rounded-lg px-4 pt-5 pb-4  shadow-xl transform transition-all sm:my-8 overflow-y-visible sm:p-6">
                 <div class="flex flex-row items-center justify-end pb-4">
-                    <button type="button"
-                        class="bg-white PopUp_Modal_Home_Close mr-4 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span class="sr-only">Close</span>
-                        <!-- Heroicon name: outline/x -->
-                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
+
                 </div>
                 <div class="flex flex-col items-center justify-center my-3 gap-y-3">
                     <div class="flex text-center justify-end gap-y-6">
                         <a target="_self"
                             class="bg-[#349A37] hover:bg-[#101426] duration-200 py-4 ml-2 text-white text-2xl rounded-[50px] w-[160px] lg:w-[180px] xl:w-[220px] "
                             href="{{ $link }}">{{ $text }}</a>
+                            <button type="button"
+                            class=" PopUp_Modal_Home_Close  bg-[#349A37] hover:bg-[#101426] duration-200 py-4 ml-2 text-white text-2xl rounded-[50px] w-[160px] lg:w-[180px] xl:w-[220px] ">
+                            <span class="sr-only">Close</span>
+                            <!-- Heroicon name: outline/x -->
+                            اكمل تصفح الموقع
+                        </button>
+
                     </div>
                     <img class="h-[1000px] hidden sm:flex "
                         src="/{{  $Imageweb }}" />
