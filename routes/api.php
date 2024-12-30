c901aa72a2947a2dd11c23477bb4c12036ce6465
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-12-30 16:28:37 +0200

    edit

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/appSettings', [TripController::class, 'getDonation']);
+Route::get('/appsettings', [TripController::class, 'getDonation']);
 
 
 
