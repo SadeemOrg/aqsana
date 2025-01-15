ffc0e22352772db39432f1459df1cb7dcc8eba54
zeyadaverotech <zeyad.hawash1@hotmail.com>
2025-01-15 13:33:46 +0200

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2579,7 +2579,6 @@ class HomeController extends BaseController
     public function getAppSettings()
     {
         $settings = DB::table('nova_settings')->pluck('value', 'key')->toArray();
-        dd($settings);
         return response()->json($settings);
     }
 }
