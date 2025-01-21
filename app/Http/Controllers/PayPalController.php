238a72f759b1e754736c6aba2bdf29eb9cfce466
zeyadaverotech <zeyad.hawash1@hotmail.com>
2025-01-21 10:02:54 +0200

    edit

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -62,8 +62,8 @@ class PayPalController extends Controller
                     ]
                 ]
             ]);
+            dd($response['links']);
             if (isset($response['id']) && $response['id'] != null) {
-                dd($response['links']);
                 foreach ($response['links'] as $links) {
                     if ($links['rel'] == 'approve') {
 
