80fc9e63aad9cd8e316d263a60f5b41c81c8c18c
zeyadaverotech <zeyad.hawash1@hotmail.com>
2025-02-01 22:23:20 +0200

    edit

--- routes/api.php
+++ routes/api.php
@@ -90,7 +90,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/delete', [AuthController::class, 'delete']);
     Route::put('/cm-firebase-token', [AuthController::class, 'update_fcm_token']);
 
-    Route::get('notifications/{user_id}', [AppNotificationController::class, 'getNotificationsByUser']);
+    Route::get('notifications', [AppNotificationController::class, 'getNotificationsByUser']);
 
 });
 
