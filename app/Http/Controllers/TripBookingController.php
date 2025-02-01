ce1f4125c9bf18ec0159e4636be4a23b4b642434
zeyadaverotech <zeyad.hawash1@hotmail.com>
2025-02-01 21:45:44 +0200

    edit

--- app/Http/Controllers/TripBookingController.php
+++ app/Http/Controllers/TripBookingController.php
@@ -111,7 +111,8 @@ class TripBookingController extends BaseController
             // }
             $userIds = [Auth()->id()];
             $title = "aqsana";
-            $body = "لقد تم  حجز قافلة . $projext->project_name. بنجاح";
+            $body = " لقد تم  حجز قافلة . $projext->project_name. بنجاح
+            .$projext->start_date. يوم";
             $notificationService = new NotificationService();
             $notificationService->sendNotification($userIds, $title, $body);
             return $this->sendResponse($tripBooking, 'تم الحجز بنجاح');
